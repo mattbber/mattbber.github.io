703ee294615ff2c2eeb6799502321021c0484bce
mattbber <119052939+mattbber@users.noreply.github.com>
2024-03-03 12:19:54 +0000

    fixed navbar bugs and implemented the portfolio into the main webpage

---
+++ js/hiddensections.js
@@ -0,0 +1,14 @@
+// Class for showing and hiding sections
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('.show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
