466c9b322657ae5bc0454f40fa65cbc5a08c7737
mattbber <119052939+mattbber@users.noreply.github.com>
2024-10-12 11:49:30 +0100

    Add files via upload

--- js/websitecontrol.js
+++ js/websitecontrol.js
@@ -10,6 +10,22 @@ window.addEventListener("scroll", function() {
     }
 });
 
+// Get the button element by its ID
+var upButton = document.getElementById("upButton");
+
+// Function to check the scroll position
+window.addEventListener("scroll", function() {
+    // Check if the user has scrolled to the top of the page
+    if (window.pageYOffset > 100) {
+        // Set the button display to 'none'
+        upButton.style.display = "block";
+    } else {
+        // Optionally, show the button if not at the top
+        upButton.style.display = "none";
+    }
+});
+
+
 var button = document.getElementById("upButton");
 
 button.addEventListener("click", function() {
@@ -28,9 +44,18 @@ document.addEventListener("keydown", e => {
     }
 });
 
-// GSAP Animations (Homepage)
-const timeline = gsap.timeline({defaults: {duration: 0.35}})
+//gsap.to('.phone', {x: '600%', y: '-100%', delay: 2});
+//gsap.to('.mail', {x: '200%', y: '500%', delay: 2});
+//gsap.to('.linkedin', {x: '-175%', y: '400%', delay: 2});
+//gsap.to('.github', {x: '-325%', y: '-200%', delay: 2});
+//gsap.to('.last', {display: 'none', delay: 2});
+//gsap.to('.matthew', {y: '700%', delay: 2});
+//gsap.to('.baber', {y: '600%', x: '75%', delay: 2});
+//gsap.to('.blink', {y: '600%', delay: 2});
 
+// GSAP Animations (Homepage)
+const timeline = gsap.timeline({defaults: {duration: 0.25}})
+    // Homepage
 timeline
     .from('.matthew', {y: '-100%',  color: 'transparent', ease: "power1.inOut"})
     .from('.baber', {y: '-100%',  color: 'transparent', ease: "power1.inOut"})
